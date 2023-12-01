0f8614d6f433d1b89d741e175d292e9cbd71032a
Igor Evtushenko <evtushwork@gmail.com>
2023-12-01 21:29:52 +0300

    less size of file

--- lcs.js
+++ lcs.js
@@ -1,31 +1,21 @@
-let input = [];
-for (let i = 2; i < process.argv.length; i++){
-    input[i - 2] = process.argv[i];
-}
-input.sort((a, b) => {return a.length - b.lenght});
-console.log(findRep(input));
-
-
-function findRep(array) {
-    let repStr = '';
-    let subStr = '';
-    let length = 1;
-    for (let i = 0; i < array[0].length;){
-        subStr = array[0].substring(i, i + length);
-        let j = 1;
-        for (; j < array.length; j++){
-            if (!array[j].includes(subStr)){
-                break;
-            }
-        }
-        if (j == array.length && subStr.length >= repStr.length){
-            repStr = subStr;
-            length++;
-        }
-        else{
-            i++;
-            length = 1;
+let array = process.argv;
+let repStr = '';
+let subStr = '';
+let length = 1;
+for (let i = 0; i < array[2].length;){
+    subStr = array[2].substring(i, i + length);
+    let j = 3;
+    for (; j < array.length; j++){
+        if (!array[j].includes(subStr)){
+            break;
         }
     }
-    return repStr;
+    if (j == array.length && subStr.length >= repStr.length){
+        repStr = subStr;
+        length++;
+    }
+    else{
+        i++;
+        length = 1;
+    }
 }
